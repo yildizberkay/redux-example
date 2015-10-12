87b7905509ce9890c30a2da024da592302c81a80
Berkay Yildiz <yildizbe@gmail.com>
2015-10-12 20:28:11 +0300

    First commit for demo page

--- src/constants/SearchActionTypes.js
+++
@@ -1,4 +0,0 @@
-export const SEARCH_PENDING = 'SEARCH_PENDING';
-export const SEARCH_PENDING_FOR_NEXT = 'SEARCH_PENDING_FOR_NEXT';
-export const SEARCH_DONE = 'SEARCH_DONE';
-export const IDLE = 'IDLE';
